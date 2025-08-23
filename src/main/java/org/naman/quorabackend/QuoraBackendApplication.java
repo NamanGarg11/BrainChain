02b5c8ebf6e7d33258878e8479b1c6e78f798ad5
Naman garg <naman.2327cseml1022@example.com>
2025-08-24 01:08:22 +0530

    changes for the required dotenv configurations

--- src/main/java/org/naman/quorabackend/QuoraBackendApplication.java
+++ src/main/java/org/naman/quorabackend/QuoraBackendApplication.java
@@ -1,5 +1,6 @@
 package org.naman.quorabackend;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class QuoraBackendApplication {
 
     public static void main(String[] args) {
+        // Load .env file
+        Dotenv dotenv = Dotenv.load();
+
+        // Make them available as System properties so Spring can resolve ${} placeholders
+        dotenv.entries().forEach(entry ->
+                System.setProperty(entry.getKey(), entry.getValue())
+        );
+
         SpringApplication.run(QuoraBackendApplication.class, args);
     }
-
 }
